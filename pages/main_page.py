fd7c8bacbab9ce5c945a3fda1a30788e93c0ac7d
Tolstolutskih <Tolstolutskih@gmail.com>
2023-05-14 16:28:08 +0300

    add initialize login page

--- pages/main_page.py
+++ pages/main_page.py
@@ -7,6 +7,6 @@ class MainPage(BasePage):
     def go_to_login_page(self):
         link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         link.click()
-        return LoginPage(browser=self.browser, url=self.browser.current_url) 
+        
     def should_be_login_link(self):
         assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), "Login link is not presented"
